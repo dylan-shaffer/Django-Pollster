d3715f57841eee77761c9e188e4edf5c7e5d352c
Dylan Shaffer <dylan.shaffer0220@gmail.com>
2022-09-12 15:01:45 -0700

    Removed dependencies that are not being used.

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.template import loader
-from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
 
 from .models import Question, Choice
